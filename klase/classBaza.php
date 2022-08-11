eff22d617a4c37d7c6a500d1a64c6a1c77dc11cc
Kovinka Markovic <51448805+Kovinkamarkovic@users.noreply.github.com>
2022-08-11 18:00:44 +0200

    Add files via upload

---
+++ klase/classBaza.php
@@ -0,0 +1,57 @@
+<?php
+class Baza{
+	private $db;
+	public function __construct()
+	{
+		$this->db=@mysqli_connect("localhost", "kovinka", "");
+		if(mysqli_connect_error())
+		{
+			return false;
+		}
+		mysqli_query($this->db, "SET NAMES UTF8");
+		return $this->db;
+	}
+	
+	public function __destruct()
+	{
+		mysqli_close($this->db);
+	}
+	
+	public function izvrsiUpit($upit)
+	{
+		return mysqli_query($this->db, $upit);
+	}
+	
+	public function brojRedova($rez)
+	{
+		if(mysqli_num_rows($rez))
+		return mysqli_num_rows($rez);
+		else
+			return "";
+	}
+	
+	public function procitajRed($rez)
+	{
+		return mysqli_fetch_object($rez);
+	}
+	
+	public function izmenjenoRedova()
+	{
+		return mysqli_affected_rows($this->db);
+	}
+	
+	public function greska()
+	{
+		return mysqli_error($this->db);
+	}
+}
+?>
+
+
+
+
+
+
+
+
+
